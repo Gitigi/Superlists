1c0a4cb0634ded32fa92eaaa1fd4bb4aaa4479b8
Gitigi <Gitigi@users.noreply.github.com>
2015-06-04 17:05:21 +0300

    Funtional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
@@ -15,18 +17,30 @@ class NewVisitorTest(unittest.TestCase):
         #she noticess the page title and header metion to-do lists
 
         self.assertIn('To-Do',self.browser.title)
-        self.fail("Finish the test!")
+
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
         #She is invited to enter a to-do item strait away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
+        
+        #she types "Buy peackock feathers" into a text box(Edith's hobby is
+        #tying fly_fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+        
+        #when she hits enter , the page updates and now the page lists
+        #1:Buy peackock feathers as an itme in a to-do list
+        inputbox.send_keys(Keys.ENTER)
 
-#she types "Buy peackock feathers" into a text box(Edith's hobby is
-#tying fly_fishing lures)
-
-#when she hits enter , the page updates and now the page lists
-#1:Buy peackock feathers as an itme in a to-do list
-
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows))
+        
 #There is still a text box inviting her to add another item.
 #She enters " Use peackock feathers to make fly" (Edith is very
 #methodical)
+        self.fail('Finish the test')
 
 #The page updates again and now shows both itmes on her list
 
