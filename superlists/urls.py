d6f193e83fac050dd16b4bcb240e88381b70c1f2
Gitigi <Gitigi@users.noreply.github.com>
2015-06-08 21:40:38 +0300

    Refactored URL

--- superlists/urls.py
+++ superlists/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 from lists import views
+from lists import urls as list_urls
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -11,7 +13,5 @@ urlpatterns = patterns('',
 
     #url(r'^admin/', include(admin.site.urls)),
     url(r'^$',views.home_page,name='home'),
-    url(r'^lists/new$',views.new_list,name='new_list'),
-    url(r'^lists/(\d+)/$',views.view_list,name='view_list'),
-    url(r'^lists/(\d+)/add_item$',views.add_item,name='add_item')
+    url(r'^lists/',include(list_urls)),
 )
