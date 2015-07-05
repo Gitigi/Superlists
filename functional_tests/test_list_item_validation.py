c1deb9822e42b32afeb4a07c63cb291cfb2eb4b5
Gitigi <stephengitigi@gmail.com>
2015-07-05 23:58:35 +0300

    duplicate validation implemented on form

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -38,12 +38,12 @@ class ItemValidationTest(FunctionalTest):
 
     def test_cannot_add_duplicate_items(self):
         #Edith goes to the home page and starts a new list
-        self.browser.get(self.server.url)
+        self.browser.get(self.server_url)
         self.get_item_input_box().send_keys("Buy wellies\n")
-        self.check_for_row_in_list_table('1:Buy wellies\n')
+        self.check_for_row_in_list_table('1:Buy wellies')
 
         self.get_item_input_box().send_keys("Buy wellies\n")
 
-        self.check_for_now_in_list_table('1:Buy wellies\n')
+        self.check_for_row_in_list_table('1:Buy wellies')
         error = self.browser.find_element_by_css_selector(".has-error")
         self.assertEqual(error.text,"You 've already got this in your list")
